79561af2a44f2ef486585eb53b0ec8ed7620f722
Alexandr Yeremieiev <sanya@hackermail.com>
2021-12-18 22:11:38 +0100

    выполнена усложненка 15

--- script.js
+++ script.js
@@ -25,7 +25,25 @@ DomElement.prototype.render = function () {
     div.style.cssText = this.cssString;
     div.textContent = this.text;
     document.body.append(div);
-
+    document.addEventListener('keydown', function (event) {
+        let domStyle = getComputedStyle(div);
+        let top = parseInt(domStyle.top);
+        let left = parseInt(domStyle.left);
+        switch (event.key) {
+            case 'ArrowUp':
+                div.style.top = top - 10 + 'px';
+                break;
+            case 'ArrowDown':
+                div.style.top = top + 10 + 'px';
+                break;
+            case 'ArrowLeft':
+                div.style.left = left - 10 + 'px';
+                break;
+            case 'ArrowRight':
+                div.style.left = left + 10 + 'px';
+                break;
+        }
+    })
 }
 
 const dom = new DomElement('.dom', '100px', '100px', 'red', '12px', 'Hello')
