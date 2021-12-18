60bf6db87117575f122ad37958be76b996e1b3e3
Alexandr Yeremieiev <sanya@hackermail.com>
2021-12-18 21:58:03 +0100

    выполнено 15 ДЗ

--- script.js
+++ script.js
@@ -1,55 +1,35 @@
-let div;
-let p;
-const DomElement = function () {
-    this.selector = function (name) {
-        if (name[0] === '.') {
-            div = document.createElement('div')
-            div.className = name.slice(1);
-            div.textContent = '123';
-            document.body.append(div);
 
-        }
-        else if (name[0] === '#') {
-            p = document.createElement('p')
-            p.id = name.slice(1);
-            p.textContent = '123';
-            document.body.append(p);
-
-        }
-    };
-
-
-    this.height = function (height) {
-        div.style.height = height
-        p.style.height = height
-    }
-
-    this.width = function (width) {
-        div.style.width = width
-        p.style.width = width
-    }
-    this.bg = function (bg) {
-        div.style.backgroundColor = bg
-        p.style.backgroundColor = bg
-    }
-    this.fontSize = function (size) {
-        div.style.fontSize = size
-        p.style.fontSize = size
-    }
+const DomElement = function (selector, height, width, bg, fontSize, text) {
+    this.selector = selector;
+    this.height = height;
+    this.width = width;
+    this.bg = bg;
+    this.fontSize = fontSize;
+    this.text = text;
+    this.cssString = `height: ${this.height};
+        width: ${this.width};
+        background-color: ${this.bg};
+        font-size: ${this.fontSize};
+        position: absolute;`
 }
+
 DomElement.prototype.render = function () {
+    // document.body.innerHTML = '';
+    let div = document.createElement('div');
+    if (this.selector[0] === '.') {
+        div.classList.add(this.selector.slice(1));
+    }
+    else if (this.selector[0] === '#') {
+        div.setAttribute('id', this.selector.slice(1));
+    }
+    div.style.cssText = this.cssString;
+    div.textContent = this.text;
+    document.body.append(div);
 
 }
 
+const dom = new DomElement('.dom', '100px', '100px', 'red', '12px', 'Hello')
+dom.render()
 
 
-const dom = new DomElement;
-dom.selector('.block')
-dom.selector('#best')
-dom.height('100px')
-dom.width('150px')
-dom.bg('red')
-dom.fontSize('32px')
-
-console.log(dom);
 
